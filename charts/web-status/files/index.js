6c6bbde5851eb0d8897b6d4f46922563f284bd32
Fer <Fernando0069@users.noreply.github.com>
2025-03-05 13:47:38 +0100

    update files

--- charts/web-status/files/index.js
+++ charts/web-status/files/index.js
@@ -20,7 +20,7 @@ const applications = [
 	{ label: 'Training', course: 'DO180', name: 'NodeJS-App' },
 	{ label: 'Training', course: 'DO180', name: 'ToDo-HTML5' },
 	{ label: 'Training', course: 'DO180', name: 'ToDo-NodeJS' },
-	{ label: 'Training', course: 'DO180', name: 'Game-Off-2012' },
+	{ label: 'Training', course: 'DO180', name: 'Alge´s-Escapade' },
 	{ label: 'Training', course: 'DO180', name: 'ToDo-NodeJS-API' }
 ];
 console.log("Aplicaciones configuradas (1):", applications);
