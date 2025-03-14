f5481256ce9597581b4645db6ce401c327b05a36
Fer <Fernando0069@users.noreply.github.com>
2025-03-14 13:27:50 +0100

    update files

--- apps/do180-weather-react/app/src/server.js
+++ apps/do180-weather-react/app/src/server.js
@@ -7,13 +7,13 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 //reroute all https traffic to http using express midleware
-app.use(function (req, res, next) {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next(); //lets request process as normal
-  }
-});
+//app.use(function (req, res, next) {
+//  if (req.headers['x-forwarded-proto'] === 'https') {
+//    res.redirect('http://' + req.hostname + req.url);
+//  } else {
+//    next(); //lets request process as normal
+//  }
+//});
 
 //tell it which folder we want to serve
 app.use(express.static('public'));
