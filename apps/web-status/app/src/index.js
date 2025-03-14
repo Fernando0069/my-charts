dc459d2d728a3c6919f595522f68c91913240913
Fer <Fernando0069@users.noreply.github.com>
2025-03-14 13:52:22 +0100

    update files

--- apps/web-status/app/src/index.js
+++ apps/web-status/app/src/index.js
@@ -22,7 +22,7 @@ const applications = [
 	{ label: 'Training', course: 'DO180', name: 'ToDo-NodeJS' },
 	{ label: 'Training', course: 'DO180', name: 'Alges-Escapade' },
 	{ label: 'Training', course: 'DO180', name: 'Retro-Games', games: ['Asteroids', 'Lunar Lander', 'Snake', 'Space Invaders'] },
-	{ label: 'Training', course: 'DO180', name: 'AntiPacMan' },
+	{ label: 'Training', course: 'DO180', name: 'AntiPacman' },
 	{ label: 'Training', course: 'DO180', name: 'O' },
 	{ label: 'Training', course: 'DO180', name: 'Tic-Tac-Toe' },
 	{ label: 'Training', course: 'DO180', name: 'Tetris-JS' },
